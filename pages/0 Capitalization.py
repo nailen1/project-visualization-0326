4cc526477424379940b02ea5fb49a733a4188343
nailen1 <45677256+nailen1@users.noreply.github.com>
2023-03-28 09:02:49 +0900

    Add files via upload

--- pages/0 Capitalization.py
+++ pages/0 Capitalization.py
@@ -8,7 +8,6 @@ def openDataFrame(path):
 
 
 df = openDataFrame("./database/auction-items-nonan.csv")
-
 st.title(f'부동산 경매 시장 전체 규모(Capitalization)')
 
 list_var_kr = ['감정가', '최저입찰가']
@@ -23,8 +22,9 @@ st.subheader(f'물건 종류별 {selected_key} 총합')
 
 selected_sido = st.selectbox('지역(시도) 선택', list_sido)
 
+df_sido = df
 if selected_sido != '전체 지역':
-    df = df[df['address_sido'] == selected_sido]
+    df_sido = df[df['address_sido'] == selected_sido]
 
 df_sorted_sido = df.sort_values(by=dict_var[selected_key], ascending=False)
 fig_a = px.bar(df_sorted_sido, y="category", x=dict_var[selected_key], color="category",
@@ -38,12 +38,13 @@ with st.expander(f'{selected_sido} 기준', expanded=True):
 
 st.subheader(f'지역별 {selected_key} 총합')
 
+df_cat = df
 selected_cat = st.selectbox('물건 종류 선택', list_cat)
 
 if selected_cat != '모든 종류':
-    df = df[df['category'] == selected_cat]
+    df_cat = df[df['category'] == selected_cat]
 
-df_sorted_cat = df.sort_values(by=dict_var[selected_key], ascending=False)
+df_sorted_cat = df_cat.sort_values(by=dict_var[selected_key], ascending=False)
 fig_b = px.bar(df_sorted_cat, y="address_sido", x=dict_var[selected_key], color="address_sido",
                color_discrete_sequence=px.colors.qualitative.G10)
 fig_b.update_xaxes(title="단위: 원")
